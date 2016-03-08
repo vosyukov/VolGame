baa4a625cc8e22a7e5da9ba5a1f5c5cd45972d7e
Vadim Osyukov <osyukov.vadim@gmail.com>
2016-03-08 11:38:38 +0300

    эксперименты над Chipmunk

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -5,8 +5,8 @@ USING_NS_CC;
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
+    auto scene = Scene::createWithPhysics();
+	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
 
@@ -16,6 +16,7 @@ Scene* HelloWorld::createScene()
     // return the scene
     return scene;
 }
+
 
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
@@ -27,6 +28,36 @@ bool HelloWorld::init()
         return false;
     }
     
+
+	_square = ui::Button::create("square_normal.png", "square_select.png");
+	_square->setPosition(Vec2(500, 400));
+	addChild(_square);
+
+	_square->addTouchEventListener(CC_CALLBACK_2(HelloWorld::ButtonCallback, this));
+
+
+	auto listener1 = EventListenerTouchOneByOne::create();
+
+	// trigger when you push down
+	listener1->onTouchBegan = [](Touch* touch, Event* event) {
+		// your code
+		return true; // if you are consuming it
+	};
+
+	// trigger when moving touch
+	listener1->onTouchMoved = [](Touch* touch, Event* event) {
+		// your code
+	};
+
+	// trigger when you let up
+	listener1->onTouchEnded = [=](Touch* touch, Event* event) {
+		// your code
+	};
+
+	// Add listener
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener1, this);
+
+
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
@@ -48,34 +79,74 @@ bool HelloWorld::init()
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    /////////////////////////////
-    // 3. add your codes below...
+	
+	//create a sprite
+	auto sprite = Sprite::create("box1.png");
+	sprite->setAnchorPoint(Vec2(0, 0));
+	sprite->setPosition(100, 600);
+	this->addChild(sprite);
+	Vec2* g = new Vec2[3];
+	g[0] = Vec2(50, 50);
+	g[1] = Vec2(50, 100);
+	g[2] = Vec2(100, 50);
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
 
-    // add the label as a child to this layer
-    this->addChild(label, 1);
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
 
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+	auto physicsBody = PhysicsBody::createPolygon(g, 3);
 
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
+		PhysicsMaterial(1.0f, 1.0f, 1.0f);
+	physicsBody->setDynamic(true);
+	//apply physicsBody to the sprite
+	sprite->addComponent(physicsBody);
+	//sprite->getPhysicsBody()->setCategoryBitmask(0x02);    // 0010
+	sprite->getPhysicsBody()->setCollisionBitmask(1);   // 0001
+	sprite->getPhysicsBody()->setContactTestBitmask(true);
+	auto sprite3 = Sprite::create("box2.png");
+	sprite3->setAnchorPoint(Vec2(0, 0));
+	sprite3->setPosition(400, 500);
+	sprite3->setColor(Color3B(100, 100, 100));
+
+
+
+	this->addChild(sprite3);
+	auto physicsBody3 = PhysicsBody::createBox(sprite3->getContentSize());
+	PhysicsMaterial(1.0f, 1.0f, 1.0f);
+	physicsBody3->setDynamic(true);
+	//apply physicsBody to the sprite
+	sprite3->addComponent(physicsBody3);
+
+
+
+
+	//create a sprite
+	auto sprite2 = Sprite::create("box4.png");
+	
+	sprite2->setAnchorPoint(Vec2(0, 0));
+	sprite2->setPosition(10, 10);
+	this->addChild(sprite2);
+	auto physicsBody2 = PhysicsBody::createBox(sprite2->getContentSize());
+
+	PhysicsMaterial(1.f, 1.0f, 1.0f);
+	physicsBody2->setDynamic(false);
+	//apply physicsBody to the sprite
+	sprite2->addComponent(physicsBody2);
+	//sprite2->getPhysicsBody()->setCategoryBitmask(0x01);    // 0001
+	sprite2->getPhysicsBody()->setCollisionBitmask(2);   // 0010
+	sprite2->getPhysicsBody()->setContactTestBitmask(true);
+	
+	auto contactListener = EventListenerPhysicsContact::create();
+	contactListener->onContactBegin = CC_CALLBACK_1(HelloWorld::onContactBegin, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);
     return true;
 }
 
-
+bool HelloWorld::onContactBegin(PhysicsContact& contact)
+{
+	auto bodyA = contact.getShapeA()->getBody();
+	auto bodyB = contact.getShapeB()->getBody();
+	return true;
+}
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
     Director::getInstance()->end();
@@ -83,4 +154,79 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
+}
+
+
+void HelloWorld::ButtonCallback(Ref *pSender, ui::Widget::TouchEventType type)
+{
+	auto button = static_cast<ui::Button*>(pSender);
+
+	switch (type)
+	{
+		case ui::Widget::TouchEventType::ENDED:
+		{
+			if (button == _square) 
+			{
+				AddShape(SelectShape::SQUARE);
+			}
+		}
+		break;
+
+		case ui::Widget::TouchEventType::CANCELED:
+		{
+
+		}
+		break;
+
+		default:
+			break;
+	}
+}
+
+void  HelloWorld::AddShape(SelectShape shape)
+{
+	switch (shape)
+	{
+		case SelectShape::SQUARE:
+		{
+			auto sprite = Sprite::create("square.png");
+			sprite->setAnchorPoint(Vec2(0, 0));
+			sprite->setPosition(280, 450);
+			sprite->setColor(Color3B(100, 100, 100));
+			this->addChild(sprite);
+			auto physicsBody = PhysicsBody::createBox(sprite->getContentSize(), PhysicsMaterial(1.0f, 1.0f, 1.0f));			
+			physicsBody->setDynamic(false);
+			sprite->addComponent(physicsBody);
+		}
+		break;
+
+		case SelectShape::RECTANGLE:
+		{
+			auto sprite = Sprite::create("square.png");
+			sprite->setAnchorPoint(Vec2(0, 0));
+			sprite->setPosition(280, 450);
+			sprite->setColor(Color3B(100, 100, 100));
+			this->addChild(sprite);
+			auto physicsBody = PhysicsBody::createBox(sprite->getContentSize(), PhysicsMaterial(1.0f, 1.0f, 1.0f));
+			physicsBody->setDynamic(true);
+			sprite->addComponent(physicsBody);
+		}
+		break;
+
+		case SelectShape::TRIANGLE:
+		{
+			auto sprite = Sprite::create("square.png");
+			sprite->setAnchorPoint(Vec2(0, 0));
+			sprite->setPosition(280, 450);
+			sprite->setColor(Color3B(100, 100, 100));
+			this->addChild(sprite);
+			auto physicsBody = PhysicsBody::createBox(sprite->getContentSize(), PhysicsMaterial(1.0f, 1.0f, 1.0f));
+			physicsBody->setDynamic(true);
+			sprite->addComponent(physicsBody);
+		}
+		break;	
+
+		default:
+			break;
+	}
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,20 +1,43 @@
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
-
+#include "ui/CocosGUI.h"
 #include "cocos2d.h"
+
+USING_NS_CC;
+enum class SelectShape {
+	SQUARE = 0,
+	TRIANGLE = 1,
+	RECTANGLE = 2
+};
 
 class HelloWorld : public cocos2d::Layer
 {
 public:
+	
     static cocos2d::Scene* createScene();
-
+	
     virtual bool init();
-    
+#if CC_ENABLE_CHIPMUNK_INTEGRATION    
+	//extension::PhysicsDebugNode* _debugLayer; // weak ref
+#endif
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+	bool onContactBegin(PhysicsContact& contact);
+
+
+	void ButtonCallback(Ref *pSender, ui::Widget::TouchEventType type);
+	void AddShape(SelectShape shape);
+
+	ui::Button* _square;
+	ui::Button* _triangle;
+	ui::Button* _rectangle;
+
+	SelectShape _selectShape;
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
